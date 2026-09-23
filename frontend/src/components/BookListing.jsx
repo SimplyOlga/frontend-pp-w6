e20bda0879f7169f9d2ed2764172be3277cb8066
Olga <olga.gonchar@metropolia.fi>
2026-09-23 23:39:43 +0300

    feat(book-page): add route and fetch single book by id

--- frontend/src/components/BookListing.jsx
+++ frontend/src/components/BookListing.jsx
@@ -1,9 +1,13 @@
+import {Link} from "react-router-dom";
+
 const BookListing = ({book}) => {
   return (
 
 
     <div className="book-preview">
-      <h2>Book Title: {book.title}</h2>
+      <Link to={`/books/${book.id}`}>
+      <h2>{book.title}</h2>
+      </Link>
       <p>Author: {book.author}</p>
       <p>ISBN: {book.isbn}</p>
       <p>Publisher: {book.publisher}</p>
--- frontend/src/pages/BookPage.jsx
+++ frontend/src/pages/BookPage.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import BookListing from "../components/BookListing";
+import { useParams } from "react-router-dom";
+
+
 const BookPage = () => {
+  const [ book, setBook ] = useState();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+  const fetchBook = async () => {
+    try {
+      const res = await fetch(`/api/books/${id}`);
+      if (!res.ok) throw new Error("not ok");
+      const data = await res.json();
+      setBook(data);
+    } catch (error) {
+      console.log(error)
+    };
+  };
+  fetchBook();
+}, []);
+
+
+
   return (
+    
     <div className="book-preview">
       <>
-        <h2></h2>
-        <p>Author: </p>
-        <p>ISBN: </p>
-        <p>Publisher: </p>
-        <p>Genre: </p>
-        <p>Available: </p>
-        <p>Due Date: </p>
-        <p>Borrower: </p>
+        {book && <BookListing book={book} />}
       </>
-    </div>
+      <button onClick={() => navigate("/")}>Back</button>
+    </div> 
   );
 };
 
