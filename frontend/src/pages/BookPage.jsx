3aa8972b912d823b6627d9bc09fe983827347e38
zidan2506 <duydan@metropolia.fi>
2026-09-23 20:47:15 +0300

    feat(book-page): add delete button with confirmation dialog

--- frontend/src/pages/BookPage.jsx
+++ frontend/src/pages/BookPage.jsx
@@ -9,6 +9,27 @@ const BookPage = () => {
   const [ book, setBook ] = useState();
   const navigate = useNavigate();
   const { id } = useParams();
+
+  const deleteBook = async(bookId) => {
+    try {
+      const res = await fetch(`/api/books/${bookId}`,
+        {method: "DELETE"}
+      );
+      if (!res.ok) throw new Error("Failed")
+    }
+  catch(error) {
+    console.error("Error deleting book:", error)
+  } 
+  };
+
+  const onDeleteClick = (bookId) => {
+    const confirm = window.confirm("Are you sure?")
+    if (!confirm) return;
+    
+    deleteBook(bookId);
+    navigate("/")
+  }
+
   useEffect(() => {
   const fetchBook = async () => {
     try {
@@ -32,6 +53,7 @@ const BookPage = () => {
         {book && <BookListing book={book} />}
       </>
       <button onClick={() => navigate("/")}>Back</button>
+      <button onClick={() => onDeleteClick(book._id)}>Delete</button>
     </div> 
   );
 };
